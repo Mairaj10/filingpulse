be6c74eeed9dd820610d81f8392e5583f8693247
Mairaj <rashdimairaj9@gmail.com>
2026-09-15 23:44:16 +0500

    feat: enable encryption on raw S3 bucket

--- infrastructure/terraform/s3.tf
+++ infrastructure/terraform/s3.tf
@@ -18,3 +18,13 @@ resource "aws_s3_bucket_versioning" "raw" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "raw" {
+  bucket = aws_s3_bucket.raw.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
