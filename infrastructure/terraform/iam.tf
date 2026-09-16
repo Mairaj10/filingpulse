113cf20b877ba28afbfd3c01d88d13eb8b8665fa
Mairaj <rashdimairaj9@gmail.com>
2026-09-16 18:24:52 +0500

    feat: add FilingPulse task IAM role

---
+++ infrastructure/terraform/iam.tf
@@ -0,0 +1,19 @@
+resource "aws_iam_role" "filingpulse_task" {
+  name = "filingpulse-task-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
